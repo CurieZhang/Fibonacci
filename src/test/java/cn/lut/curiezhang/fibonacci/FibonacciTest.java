c006048e0928f1c27e067564a1fb5f6a2fc8408f
curiezhang <zhjl@lut.cn>
2019-08-30 17:25:42 +0800

    增加测试用例{2，1}，成功，{3，2}失败

--- src/test/java/cn/lut/curiezhang/fibonacci/FibonacciTest.java
+++ src/test/java/cn/lut/curiezhang/fibonacci/FibonacciTest.java
@@ -10,7 +10,7 @@ public class FibonacciTest {
 	public void testFibonacci() {
 		//assertEquals(0, Fibonacci.fib(0));
 		//assertEquals(1, Fibonacci.fib(1));
-		int cases[][] = {{0, 0}, {1, 1}};
+		int cases[][] = {{0, 0}, {1, 1}, {2, 1}, {3, 2}};
 			for(int i = 0; i < cases.length; i++) {
 				assertEquals(cases[i][1], Fibonacci.fib(cases[i][0]));
 		}
